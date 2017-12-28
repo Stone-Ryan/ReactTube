a6f6682ca6d3dfcdc29d35b12b47863a03cfa932
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-27 21:08:15 -0800

    added component file and dummy folders, started to build out SearchBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function() {
-  return <div>Hi!</div>;
+import SearchBar from './components/SearchBar';
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>);
 };
+
+ReactDOM.render(<App />, document.querySelector('.container'));
