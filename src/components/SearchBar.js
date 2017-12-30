8d9058037d59e26d934cedf3d9a22f0105a4115c
SDRyan9 <stonedouglasryan@gmail.com>
2017-12-29 18:27:33 -0800

    fixed some spacing to be more consistent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,11 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = {
-      term: ''
-    };
+    this.state = { term: '' };
   }
 
   onInputChange(term) {
-    this.setState({term});
+    this.setState({ term });
     this.props.onSearchTermChange(term)
   }
 
